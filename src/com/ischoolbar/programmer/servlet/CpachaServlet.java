d52727df865d3a880c06c3073f46e02a15807e83
wutao <wutao@zhijiesong.com>
2020-02-10 21:05:04 +0800

    初始化

---
+++ src/com/ischoolbar/programmer/servlet/CpachaServlet.java
@@ -0,0 +1,42 @@
+package com.ischoolbar.programmer.servlet;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ischoolbar.programmer.util.CpachaUtil;
+/**
+ * 
+ * @author
+ *验证码servlet
+ */
+public class CpachaServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4919529414762301338L;
+	public void doGet(HttpServletRequest request,HttpServletResponse reponse) throws IOException{
+		doPost(request, reponse);//调用post方法
+	}
+	public void doPost(HttpServletRequest request,HttpServletResponse reponse) throws IOException{
+		String method = request.getParameter("method");
+		if("loginCapcha".equals(method)){
+			generateLoginCpacha(request, reponse);
+			return;
+		}
+		reponse.getWriter().write("error method");
+	}
+	//调用验证码方法
+	private void generateLoginCpacha(HttpServletRequest request,HttpServletResponse reponse) throws IOException{
+		CpachaUtil cpachaUtil = new CpachaUtil();//
+		String generatorVCode = cpachaUtil.generatorVCode();//生成验证码
+		request.getSession().setAttribute("loginCapcha", generatorVCode);
+		BufferedImage generatorRotateVCodeImage = cpachaUtil.generatorRotateVCodeImage(generatorVCode, true);
+		ImageIO.write(generatorRotateVCodeImage, "gif", reponse.getOutputStream());
+	}
+}
